99d69a3140f2fb1e7bdc081e8ea0853bf1ed8cc1
denny-lee <zhuimeng12321@gmail.com>
2017-10-02 17:14:02 +0800

    test spring ws

--- web/src/main/java/com/lee/controller/WsController.java
+++ web/src/main/java/com/lee/controller/WsController.java
@@ -1,21 +1,26 @@
 package com.lee.controller;
 
-import com.lee.OrderStatus;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-
-import java.security.Principal;
 
 @Controller
 public class WsController {
+    private static final Logger logger = LoggerFactory.getLogger(WsController.class);
 
     @MessageMapping("/hello/{topic}")
-    @SendToUser("/queue/hello")
-    public OrderStatus send(@DestinationVariable("topic") String topic, OrderStatus orderStatus, Principal principal) throws Exception {
-        System.out.println("===========topic:"+topic);
-        System.out.println("===========userName:"+ principal.getName());
-        return new OrderStatus(orderStatus.getOrderNo(), topic);
+    @SendTo("/queue/hello")
+//    @SendToUser("/queue/hello")
+    public String send(@DestinationVariable("topic") String topic, String str) throws Exception {
+        logger.info("topic:{}", topic);
+        if (StringUtils.isNotBlank(str)) {
+            return "echo back "+ str;
+        } else {
+            return "你什么也没传";
+        }
     }
 }

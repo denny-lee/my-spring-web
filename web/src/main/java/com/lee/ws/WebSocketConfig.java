caad3291f32959ddc1a6b0bd98612707ff254a7c
denny-lee <zhuimeng12321@gmail.com>
2017-10-04 22:45:06 +0800

    event listener

--- web/src/main/java/com/lee/ws/WebSocketConfig.java
+++ web/src/main/java/com/lee/ws/WebSocketConfig.java
@@ -1,17 +1,22 @@
 package com.lee.ws;
 
-import org.springframework.context.annotation.Bean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 //import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 
 
 @Configuration
 @EnableWebSocketMessageBroker
 //@EnableWebSocket
-public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer implements ApplicationListener<SessionDisconnectEvent> {
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketConfig.class);
     /*@Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
         webSocketHandlerRegistry.addHandler(myHandler(), "/myHandler").withSockJS();
@@ -21,6 +26,11 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     public WebSocketHandler myHandler() {
         return new MyHandler();
     }*/
+
+    @Override
+    public void onApplicationEvent(SessionDisconnectEvent event) {
+        logger.info(event.toString());
+    }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
